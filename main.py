d6c0e979fa65b8ae23112a73aa2b0be03fb498b0
carocg09 <carolinaceballos211@gmail.com>
2024-12-08 10:07:52 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+name = input("¿Como te llamas?")
+print("Hola", name, "bienvenid@ a tu diccionario moderno!")
+             
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado"
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Perdón, no encuentro esa palabra en mi contenido, intenta con otra")
